69029103e4d6b24a132f71c923ac502bb16351f3
bixad2005 <34238@iitu.edu.kz>
2025-02-05 09:24:49 +0500

    Step12

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,17 +1,19 @@
 import React from "react";
 import TodoListItems from "./TodoListItems";
+import './TodoList.css'
 
 const TodoList = ({ todos}) => {
     const elements = todos.map((item) => {
         const {id, ...itemProps } = item;
+
         return (
-            <li key = {item.id}>
+            <li key={id} className={"list-group-item"}>
                 <TodoListItems {...itemProps}/>
             </li>
         );
     });
     return (
-        <ul>
+        <ul className={"list-group todo-list"}>
             {elements}
         </ul>
     );
--- src/Components/TodoListItems.js
+++ src/Components/TodoListItems.js
@@ -1,11 +1,14 @@
 import React from "react";
+import './TodoListItems.css'
 
 const TodoListItems = ({ label, important = false }) => {
 
     const style = {
         color: important ? "red" : "black",
     }
-    return <span style={style}>{label}</span>
+    return <span className = {"todo-list-item"}
+            style = {style}> {label}
+    </span>
 };
 
 export default TodoListItems;
