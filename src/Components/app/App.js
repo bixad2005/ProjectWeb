79c25d44428813252dff1d1b8c68c56499ed6a85
bixad2005 <34238@iitu.edu.kz>
2025-02-12 12:39:02 +0500

    Step29

--- src/Components/app/App.js
+++ src/Components/app/App.js
@@ -44,7 +44,14 @@ export default class App extends Component {
                 todoData: newArr
             };
         });
-    }
+    };
+
+    onToggleImportant = (id) => {
+        console.log('Toggle Important', id);
+    };
+    onToggleDone = (id) => {
+        console.log('Toggle Done', id);
+    };
     render(){
         return(
             <div className="todo-app">
@@ -54,7 +61,10 @@ export default class App extends Component {
                     <ItemStatusFilter />
                     </div>
                 <TodoList todos={this.state.todoData}
-                          onDeleted = {this.deleteItem}/>
+                          onDeleted = {this.deleteItem}
+                          onToggleImportant = {this.onToggleImportant}
+                          onToggleDone = {this.onToggleDone}
+                />
                 <ItemAddForm onItemAdd = {this.addItem}/>
             </div>
         );
--- src/Components/todo-list/TodoList.js
+++ src/Components/todo-list/TodoList.js
@@ -2,14 +2,17 @@ import React from "react";
 import TodoListItems from "../todo-list-item/TodoListItems";
 import './TodoList.css'
 
-const TodoList = ({ todos, onDeleted}) => {
+const TodoList = ({ todos, onDeleted, onToggleDone, onToggleImportant}) => {
     const elements = todos.map((item) => {
         const {id, ...itemProps } = item;
 
         return (
             <li key={id} className="list-group-item">
                 <TodoListItems {...itemProps}
-                onDeleted={() => onDeleted(id)}/>
+                onDeleted={() => onDeleted(id)}
+                onToggleImportant={() => onToggleImportant(id)}
+                onToggleDone={() => onToggleDone(id)}
+                />
             </li>
         );
     });
