62236ecea0887b26d25ffcc6b5f9fc69849f5783
bixad2005 <34238@iitu.edu.kz>
2025-02-12 10:41:28 +0500

    Step24

--- src/Components/todo-list/TodoList.js
+++ src/Components/todo-list/TodoList.js
@@ -2,13 +2,14 @@ import React from "react";
 import TodoListItems from "../todo-list-item/TodoListItems";
 import './TodoList.css'
 
-const TodoList = ({ todos}) => {
+const TodoList = ({ todos, onDeleted}) => {
     const elements = todos.map((item) => {
         const {id, ...itemProps } = item;
 
         return (
             <li key={id} className="list-group-item">
-                <TodoListItems {...itemProps}/>
+                <TodoListItems {...itemProps}
+                onDeleted={() => onDeleted(id)}/>
             </li>
         );
     });
